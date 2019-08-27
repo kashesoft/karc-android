a8299e986fd6a162120601b483f5ca5edeeb1923
Andrew Kashaed <kashesoft@gmail.com>
2019-08-27 13:17:19 -0500

    Fixed deadlock

--- lib/src/main/java/com/kashesoft/karc/core/interactor/Interaction.kt
+++ lib/src/main/java/com/kashesoft/karc/core/interactor/Interaction.kt
@@ -15,36 +15,29 @@ abstract class Interaction<R> constructor(
     private var interactable: Interactable? = null
     private val listeners: MutableList<InteractionListener<R>> = mutableListOf()
 
-    @Synchronized
     fun start() {
         onInteractionStatus(InteractionStatus.Start(this))
     }
 
-    @Synchronized
     fun succeed(data: R) {
         onInteractionStatus(InteractionStatus.Success(this, data))
     }
 
-    @Synchronized
     fun finish() {
         onInteractionStatus(InteractionStatus.Finish(this))
         onInteractionStatus(InteractionStatus.Stop(this))
     }
 
-    @Synchronized
     fun fail(error: Throwable) {
         error.printStackTrace()
         onInteractionStatus(InteractionStatus.Failure(this, error))
         onInteractionStatus(InteractionStatus.Stop(this))
     }
 
-    @Synchronized
     fun cancel() {
-        if (interactable?.isCanceled() == false) {
-            interactable?.cancel()
-            onInteractionStatus(InteractionStatus.Cancel(this))
-            onInteractionStatus(InteractionStatus.Stop(this))
-        }
+        if (!cancelIfNeeded()) return
+        onInteractionStatus(InteractionStatus.Cancel(this))
+        onInteractionStatus(InteractionStatus.Stop(this))
     }
 
     @Synchronized
@@ -63,29 +56,44 @@ abstract class Interaction<R> constructor(
         listeners.remove(listener)
     }
 
-    @Synchronized
-    protected fun onInteractionStatus(interactionStatus: InteractionStatus<R>) {
-        val interactor = this.interactor.get() ?: return
+    private fun onInteractionStatus(interactionStatus: InteractionStatus<R>) {
+        val interactor = synchronized(this) { this.interactor.get() ?: return }
         if (interactionStatus is InteractionStatus.Start) {
-            try {
-                interactable = generateInteractable()
-            } catch (error: Throwable) {
-                error.printStackTrace()
-                publishInteractionStatus(InteractionStatus.Failure(this, error))
-                return
+            synchronized(this) {
+                try {
+                    interactable = generateInteractable()
+                } catch (error: Throwable) {
+                    error.printStackTrace()
+                    publishInteractionStatus(InteractionStatus.Failure(this, error))
+                    return
+                }
             }
             interactor.attachInteraction(this as Interaction<Any>)
         }
-        publishInteractionStatus(interactionStatus)
+        synchronized(this) {
+            publishInteractionStatus(interactionStatus)
+        }
         if (interactionStatus is InteractionStatus.Stop) {
             interactor.detachInteraction(this as Interaction<Any>)
-            interactable = null
+            synchronized(this) {
+                interactable = null
+            }
         }
     }
 
     private fun publishInteractionStatus(interactionStatus: InteractionStatus<R>) {
         for (listener in listeners) {
             listener.onInteractionStatus(interactionStatus)
+        }
+    }
+
+    @Synchronized
+    private fun cancelIfNeeded(): Boolean {
+        return if (interactable?.isCanceled() == false) {
+            interactable?.cancel()
+            true
+        } else {
+            false
         }
     }
 
